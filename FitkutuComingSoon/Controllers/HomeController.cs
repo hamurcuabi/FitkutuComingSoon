d5f0146293c5ccdf3adef7270631613e5c8f4c88
Emre Hamurcu <emrehamurcu_1995@hotmail.com>
2019-10-09 12:31:51 +0300

    Add project files.

---
+++ FitkutuComingSoon/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using MailLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FitkutuComingSoon.Controllers
+{
+    public class HomeController : Controller
+    {
+        Mail Mail;
+
+        public HomeController()
+        {
+            Mail = new Mail();
+        }
+
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult EmailSender()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EmailSender(string email)
+        {
+            Mail.SendMail(email);
+            return View();
+        }
+    }
+}
---
+++ MailLib/Mail.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailLib
+{
+  public  class Mail
+    {
+        public void SendMail(String email) {
+            SmtpClient client = new SmtpClient();
+            MailAddress from = new MailAddress("emirogs1@gmail.com");
+            MailAddress to = new MailAddress("devim.celik@motion-steel.com");//bizim mail adresi
+            MailMessage msg = new MailMessage(from, to);
+            msg.IsBodyHtml = true;
+            msg.Subject = "Fitkutu Takip Et";
+            msg.Body += "<h2> " + email + " </h2> adresli kişi Fitkutuyu takip etmek istiyor "; //burada başında gönderen kişinin mail adresi geliyor
+            msg.CC.Add("fitkutu@fitkutu.com");//herkes görür
+            NetworkCredential info = new NetworkCredential("emirogs1@gmail.com", "Emrhmrc.95");
+            client.Port = 587;
+            client.Host = "smtp.gmail.com";
+            client.EnableSsl = true;
+            client.Credentials = info;
+            client.Send(msg);
+         
+        }
+    }
+}
